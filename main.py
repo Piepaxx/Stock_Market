f8b308a50a3464cb4b604db96addb74760995ac1
Paul Iacoblev <Pauliacoblev3@gmail.com>
2025-11-03 22:11:03 +0200

    function: message output if most recent closing is +-5% from previous day closing price

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta
 
 
+
 load_dotenv()
 STOCK_API_KEY = os.environ.get("ALPHA_VANTAGE_API_KEY")
 url = 'https://www.alphavantage.co/query?'
@@ -17,6 +18,7 @@ params = {
 #Getting the STOCK DATA
 resource = requests.get(url, params= params)
 resource.raise_for_status()
+print(resource.json())
 
 #Get the previous day
 last_refresh = resource.json()["Meta Data"]["3. Last Refreshed"]
@@ -24,5 +26,16 @@ date_obj = datetime.strptime(last_refresh, "%Y-%m-%d")
 previous_day_time = date_obj - timedelta(days=1)
 previous_day = previous_day_time.strftime("%Y-%m-%d")
 
-stock_data = resource.json()["Time Series (Daily)"][last_refresh]
-print(stock_data)
+stock_data = resource.json()["Time Series (Daily)"]
+
+last_stock_data = stock_data[last_refresh]
+last_closing = last_stock_data["4. close"]
+
+previous_day_data = stock_data[previous_day]
+previous_day_closing = previous_day_data["4. close"]
+
+change = float(previous_day_closing) - float(last_closing)
+percent_change = change/100 * previous_day_closing
+
+if 5 > percent_change > -5:
+    print("HUGE STOCK DIFFERENCE FOR TESLA")
